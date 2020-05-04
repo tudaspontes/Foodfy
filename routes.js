b79f5472c37ec1dff369871b7988cc39cb9569be
Felipe <felipedemelopontes@gmail.com>
2020-05-03 21:18:47 -0300

    feat: create page style edited

--- routes.js
+++ routes.js
@@ -7,21 +7,29 @@ const recipes = require("./data")
 routes.get('/', function(req, res) {
     const featuredRecipes = recipes.slice(0, 6);
 
-    res.render('home', { recipes: featuredRecipes});
+    res.render('./admin/index', { recipes: featuredRecipes});
 });
 
 routes.get('/about', function(req, res) {
-    res.render('about');
+    res.render('./admin/about');
 });
 
 routes.get('/recipes', function(req, res) {
-    res.render('recipes', { recipes });
+    res.render('./admin/recipes', { recipes });
 });
 
 routes.get('/recipes/:id', function(req, res) {
     const id = req.params.id;
 
-    res.render('recipe', { recipe: recipes[id] })
+    res.render('./admin/recipe', { recipe: recipes[id] })
+})
+
+routes.get('/admin', function (req, res) {
+    res.render('./admin/create')
+})
+
+routes.post('/admin', function (req, res) {
+    res.send('recebido')
 })
 
 // routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
